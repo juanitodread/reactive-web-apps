6bab3dc211d63c94fb2180426cb2b365cfb2bfb3
Juan Sandoval <juanitodread@gmail.com>
2016-09-09 17:12:32 -0500

    Hello world play

---
+++ reactive-streams/app/controllers/Application.scala
@@ -0,0 +1,40 @@
+/**
+ * reactive-streams
+ *
+ * Copyright 2016 juanitodread
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package controllers
+
+import play.api.Logger
+
+import play.api.mvc.Action
+import play.api.mvc.Controller
+
+/**
+ * Default app controller
+ *
+ * @author juanitodread
+ *
+ */
+class Application extends Controller {
+  final val logger = Logger(this.getClass)
+
+  def index = Action { request =>
+    logger.info("Entry to index")
+
+    Ok("Hello World :)")
+  }
+}
